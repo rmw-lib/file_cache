5725bd48a422856136d91a4fc0678625c541c00d
gcxfd <i@rmw.link>
2022-07-12 16:30:56 +0800

    🔶

--- examples/main.rs
+++ examples/main.rs
@@ -1,10 +1,15 @@
 use anyhow::Result;
+use async_std::io::{prelude::SeekExt, ReadExt, SeekFrom};
 use file_cache::FileCache;
 
 async fn get(cache: &mut FileCache<'_>) -> Result<()> {
   let path = "/Users/z/rmw/file_handle_cache/1.txt";
   let host = cache.get(path.as_bytes()).await?;
-  dbg!(host);
+  let mut host = host.value();
+  host.seek(SeekFrom::Start(0)).await?;
+  let mut out = [0u8; 1024];
+  let n = host.read(&mut out).await?;
+  dbg!(std::str::from_utf8(&out[..n])?);
   Ok(())
 }
 
