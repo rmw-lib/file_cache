50f5e37843e2fd074e751620aea961dc3305bba4
gcxfd <i@rmw.link>
2022-07-12 13:19:46 +0800

    🔶

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,5 @@
-use std::hash::Hash;
-
-use ahash::AHasher;
-use async_std::file::File;
+use async_std::io::File;
 use stretto::{AsyncCache, CacheError, TransparentKeyBuilder};
-
-#[derive(Debug, Default)]
-struct Hasher(AHasher);
-
-impl<K: Hash + Eq + ?Sized> stretto::KeyBuilder<K> for Hasher {
-  fn hash_index(&self, key: &K) -> u64 {
-    let mut hasher = AHasher::default();
-    key.hash(hasher);
-    hasher.finish()
-  }
-}
 
 pub struct FileCache {}
 
@@ -21,12 +7,6 @@ impl FileCache {
   pub fn new() -> Result<Self, CacheError> {
     let open_limit = err::ok!(file_open_limit::get()).unwrap() / 2;
     let num_counters = open_limit * 128;
-    AsyncCache::new_with_key_builder(
-      num_counters,
-      open_limit,
-      Hasher::default(),
-      async_std::task::spawn,
-    )
-    .unwrap();
+    AsyncCache::new(num_counters, open_limit as _, async_std::task::spawn).unwrap();
   }
 }
