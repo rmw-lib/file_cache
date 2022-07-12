492dfb304786bf904e6abe5dc4e251ac7dc96ed1
gcxfd <i@rmw.link>
2022-07-12 16:45:17 +0800

    🔶

--- examples/main.rs
+++ examples/main.rs
@@ -2,14 +2,19 @@ use anyhow::Result;
 use async_std::io::{prelude::SeekExt, ReadExt, SeekFrom};
 use file_cache::FileCache;
 
-async fn get(cache: &mut FileCache<'_>) -> Result<()> {
-  let path = "/Users/z/rmw/file_handle_cache/1.txt";
-  let host = cache.get(path.as_bytes()).await?;
+async fn get(cache: &mut FileCache) -> Result<()> {
+  let mut path = std::env::current_exe()?;
+  (0..4).for_each(|_| {
+    path.pop();
+  });
+  let path = path.join("Cargo.toml").display().to_string();
+  let host = cache.get(path).await?;
   let mut host = host.value();
   host.seek(SeekFrom::Start(0)).await?;
   let mut out = [0u8; 1024];
   let n = host.read(&mut out).await?;
-  dbg!(std::str::from_utf8(&out[..n])?);
+  println!("{}", std::str::from_utf8(&out[..n])?);
+  dbg!(n);
   Ok(())
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ use async_std::{fs::File, path::Path};
 use stretto::{AsyncCache, CacheError, ValueRefMut};
 
 #[derive(Clone)]
-pub struct FileCache<'a> {
-  cache: AsyncCache<&'a [u8], File>,
+pub struct FileCache {
+  cache: AsyncCache<Box<[u8]>, File>,
 }
 
-impl<'a> FileCache<'a> {
+impl FileCache {
   pub fn new(open_limit: usize) -> Result<Self, CacheError> {
     let open_limit = open_limit.min(err::ok!(file_open_limit::get()).unwrap() / 2);
     let num_counters = open_limit * 128;
@@ -16,12 +16,13 @@ impl<'a> FileCache<'a> {
     })
   }
 
-  pub async fn get(&mut self, path: &'a [u8]) -> async_std::io::Result<ValueRefMut<File>> {
+  pub async fn get(&mut self, path: impl AsRef<[u8]>) -> async_std::io::Result<ValueRefMut<File>> {
+    let path = Box::from(path.as_ref());
     if let Some(exist) = self.cache.get_mut(&path) {
       return Ok(exist);
     }
-    let file = File::open(Path::new(unsafe { std::str::from_utf8_unchecked(path) })).await?;
-    self.cache.insert(path, file, 1).await;
+    let file = File::open(Path::new(unsafe { std::str::from_utf8_unchecked(&path) })).await?;
+    self.cache.insert(path.clone(), file, 1).await;
     self.cache.wait().await.unwrap();
     Ok(self.cache.get_mut(&path).unwrap())
   }
