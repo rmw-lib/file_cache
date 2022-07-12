4e5fa7e3d7764f48343c0c297fc2d0834ebc370a
gcxfd <i@rmw.link>
2022-07-12 16:12:18 +0800

    🔶

--- examples/main.rs
+++ examples/main.rs
@@ -1,6 +1,11 @@
 use anyhow::Result;
+use file_handle_cache::FileCache;
 
-fn main() -> Result<()> {
-  let cache = file_handle_cache::FileCache::new(2048);
+#[async_std::main]
+async fn main() -> Result<()> {
+  let mut cache = file_handle_cache::FileCache::new(2048)?;
+  let path = "/Users/z/rmw/file_handle_cache/1.txt";
+  let host = cache.get(path.as_bytes()).await?;
+  dbg!(host);
   Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
-use std::ffi::OsStr;
-
 use async_std::{fs::File, path::Path};
 use stretto::{AsyncCache, CacheError, ValueRefMut};
 
 #[derive(Clone)]
-pub struct FileCache {
-  cache: AsyncCache<Box<[u8]>, File>,
+pub struct FileCache<'a> {
+  cache: AsyncCache<&'a [u8], File>,
 }
 
-impl FileCache {
+impl<'a> FileCache<'a> {
   pub fn new(open_limit: usize) -> Result<Self, CacheError> {
     let open_limit = open_limit.min(err::ok!(file_open_limit::get()).unwrap() / 2);
     let num_counters = open_limit * 128;
@@ -18,12 +16,13 @@ impl FileCache {
     })
   }
 
-  pub async fn open(&mut self, path: Box<[u8]>) -> async_std::io::Result<ValueRefMut<File>> {
+  pub async fn get(&mut self, path: &'a [u8]) -> async_std::io::Result<ValueRefMut<File>> {
     if let Some(exist) = self.cache.get_mut(&path) {
       return Ok(exist);
     }
-    let file = File::open(Path::new(unsafe { std::str::from_utf8_unchecked(&path) })).await?;
-    self.cache.insert(path.clone(), file, 1).await;
+    let file = File::open(Path::new(unsafe { std::str::from_utf8_unchecked(path) })).await?;
+    self.cache.insert(path, file, 1).await;
+    self.cache.wait().await.unwrap();
     Ok(self.cache.get_mut(&path).unwrap())
   }
 }
