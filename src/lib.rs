d99dd4aff36cc917ebd101f6f621e715eb3bda4d
gcxfd <i@rmw.link>
2022-07-12 13:21:01 +0800

    🔶

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 use async_std::fs::File;
 use stretto::{AsyncCache, CacheError, TransparentKeyBuilder};
 
-pub struct FileCache {}
+pub struct FileCache {
+  cache: u8,
+}
 
 impl FileCache {
   pub fn new() -> Result<Self, CacheError> {
     let open_limit = err::ok!(file_open_limit::get()).unwrap() / 2;
     let num_counters = open_limit * 128;
-    AsyncCache::new(num_counters, open_limit as _, async_std::task::spawn).unwrap();
+    Ok(Self {
+      cache: AsyncCache::new(num_counters, open_limit as _, async_std::task::spawn)?,
+    })
   }
 }
